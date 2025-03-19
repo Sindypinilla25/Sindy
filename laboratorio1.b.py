740584bbd24cfad74e517c8c428704d718e014c8
sindy <sindypinilla224@unisangil.edu.co>
2025-03-18 20:07:03 -0500

    laboratorio 1 punto 2

---
+++ laboratorio1.b.py
@@ -0,0 +1,10 @@
+#punto b
+
+import math
+
+radio = float(input("ingrese el radio de la esfera: "))
+
+volumen = (4/3) * math.pi * (radio ** 3)
+
+#mostrar el resultado
+print(f"el volumen de la esfera con radio {radio} es: {volumen:.2f} ")
