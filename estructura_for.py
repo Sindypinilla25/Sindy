dba072f120e0de1e507a66943f165d0f5b8fa05f
sindy <sindypinilla224@unisangil.edu.co>
2025-04-02 14:27:39 -0500

    estructura for con range

---
+++ estructura_for.py
@@ -0,0 +1,31 @@
+#estructura for
+for i in range(2):
+    print(i)
+
+for i in range(10):
+    print(i)
+
+#estructura for con range
+for i in range(1, 10):
+    print(i)
+
+#estructurafor con range
+
+for i in range(3, 10,2):
+    print(i)
+
+# variable 
+x = "python"
+
+#separar los caracteres con for
+for letras in x:
+    if letras == "h":
+        break
+    else:
+        print(letras)
+    print(letras)
+
+# ejemplo 1
+for mul in range(1,11):
+    print(f"2 x {mul} = {2 * mul}")
+
